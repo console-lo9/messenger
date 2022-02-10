1b3086b5d70c1c2efa1aba299378b976cc6b2bd5
박중섭 <>
2022-02-11 08:52:14 +0900

    Modify: 페이지 구조 추가 및 CSS 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import useFetch from 'hooks/useFetch';
 import { Fragment, useEffect } from 'react';
-
 import GlobalStyle from './GlobalStyle';
 import Main from 'pages/Main';
+import { FETCH } from 'store';
 import { useDispatch, useSelector } from 'react-redux';
-import { FETCH } from 'store/message';
 
 function App() {
     const dispatch = useDispatch();
+    const state = useSelector((state) => state.message);
+
     const fetchData = useFetch('http://localhost:4000/messages');
 
     useEffect(() => {
-        dispatch({
-            type: FETCH,
-            value: fetchData,
-        });
+        if (fetchData) {
+            dispatch({
+                type: FETCH,
+                value: fetchData,
+            });
+        }
     }, [fetchData]);
 
-    if (!fetchData) return <div>로딩중</div>;
+    if (!state) return <div>로딩중</div>;
     return (
         <Fragment>
             <GlobalStyle />
