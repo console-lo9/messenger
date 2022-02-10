38ede057017b07fe6634136a5b41844196d3e6ca
Ubermensch0608 <melancholyee28@gmail.com>
2022-02-10 20:46:51 +0900

    Fix: 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import GlobalStyle from './GlobalStyle';
+import Main from 'pages/Main';
 
 function App() {
     return (
         <Fragment>
             <GlobalStyle />
-            <div>App</div>
+            <Main />
         </Fragment>
     );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,41 @@
 import { createStore } from 'redux';
 
-const todoReducer = (state, action) => {
-    if (action.type === 'remove') {
-        return {
-            todoId: action.id,
-        };
-    }
+const ADD_MESSAGE = 'ADD_MESSAGE';
+const REPLY_MESSAGE = 'REPLY_MESSAGE';
+const REMOVE_MESSAGE = 'REMOVE_MESSAGE';
 
-    return state;
+export const addMessage = () => ({ type: ADD_MESSAGE });
+export const replyMessage = () => ({ type: REPLY_MESSAGE });
+export const removeMessage = () => ({ type: REMOVE_MESSAGE });
+
+const todoReducer = (state = { initialData: 0 }, action) => {
+    switch (action.type) {
+        case ADD_MESSAGE:
+            return [
+                ...state,
+                {
+                    userId: action.id,
+                    userName: action.name,
+                    profileImage: action.profileImage,
+                    content: action.content,
+                    date: Date.now(),
+                },
+            ];
+
+        case REPLY_MESSAGE:
+            return [
+                ...state,
+                {
+                    userName: action.name,
+                    content: action.content,
+                },
+            ];
+        case REMOVE_MESSAGE:
+            return state;
+
+        default:
+            return state;
+    }
 };
 
 const store = createStore(todoReducer);
